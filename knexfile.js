109c092c7a551e4d328389be4d7580f67a6145de
Marie Hughes <mariehughes900@gmail.com>
2016-10-10 11:09:48 -0500

    Added config to package.json and pushed to CF

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     connection: {
       host: '127.0.0.1',
       user: 'root',
-      password: 'L1mavady',
+      password: '',
       database: 'pingPong_development'
     },
     pool: {
